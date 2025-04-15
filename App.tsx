1ea15d8ff107c0da7f4e3aa7d7d53de8de3d4b73
Le Dinh Phuc <phucld@ominext.com>
2025-04-15 17:29:49 +0700

    first commit

--- App.tsx
+++ App.tsx
@@ -1,19 +1,16 @@
-import { StatusBar } from 'expo-status-bar';
+import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-
+import { SafeAreaProvider } from 'react-native-safe-area-context';
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaProvider></SafeAreaProvider>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#f0f8ff',
     alignItems: 'center',
     justifyContent: 'center',
   },
