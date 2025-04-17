b228273177c5e7a04460addb9e013c96235f58c6
Lauv Edward <edwardlauv@gmail.com>
2025-04-17 22:12:13 +0700

    Fix UI home

--- app/home.tsx
+++ app/home.tsx
@@ -2,32 +2,70 @@ import {Text, View, StyleSheet, FlatList, TouchableOpacity} from "react-native";
 import {Key, useState} from "react";
 import Octicons from "@expo/vector-icons/Octicons";
 import Colors from "../components/colors"
-
+import Icon from 'react-native-vector-icons/EvilIcons';
 
 // @ts-ignore
 export default function Home({navigation}) {
   const [activity, setActivity] = useState([
-    {icon: "💧", name: "Tập thể dục", day: ["T2", "T3"]},
-    {icon: "💧", name: "Đọc sách", day: ["T2", "T3"]},
-    {icon: "💧", name: "Uống đủ nước", day: ["T2", "T3"]},
+    {icon: "💧", name: "Tập thể dục", day: ['T2', 'T3', 'T4', 'T5']},
+    {icon: "💧", name: "Đọc sách", day: ['T5', 'T6', 'T7', 'CN']},
+    {icon: "💧", name: "Uống đủ nước", day: ['T6', 'T7', 'CN']}
   ])
   // @ts-ignore
   const renderItem = ({item}) => (
-    <View style={styles.list}>
-      <View style={{flexDirection: 'row'}}>
-        <Text>{item.icon}</Text>
-        <Text style={{fontSize: 17, fontWeight: "500"}}>{item.name}</Text>
+    <TouchableOpacity>
+      <View style={styles.list}>
+        <View style={{flexDirection: 'row', justifyContent: "space-between", alignItems: "center"}}>
+          <View style={{flexDirection: 'row', alignItems: "center"}}>
+            <Text style={{fontSize: 25, padding: 5}}>{item.icon}</Text>
+            <View>
+              <Text style={{fontSize: 17}}>{item.name}</Text>
+              <View style={{flexDirection: 'row', alignItems: "center", padding: 5}}>
+                {item.day.map((dayItem: String, index: Key) => (
+                  <Text key={index}
+                        style={{backgroundColor: "#d3d3d3", padding: 5, marginEnd: 5, borderRadius: 5, fontSize: 8}}
+                  >{dayItem}</Text>
+                ))}
+              </View>
+            </View>
+          </View>
+          <Icon name="clock" size={30}/>
+        </View>
       </View>
-      {item.day.map((dayItem: String, index: Key) => (
-        <Text key={index}
-              style={{backgroundColor: "red"}}
-        >{dayItem}</Text>
-      ))}
-    </View>
+    </TouchableOpacity>
   );
   return (
     <View style={styles.container}>
       <Text style={styles.textHeader}>Activity</Text>
+      <View style={{
+        padding: 10,
+        backgroundColor: "#fff",
+        borderRadius: 10,
+        marginBottom: 20,
+        flexDirection: "row",
+        alignItems: "center",
+        justifyContent: "space-between"}}>
+        {['T2', 'T3', 'T4', 'T5', 'T6', 'T7', 'CN'].map((item, index) => (
+          <>
+            <TouchableOpacity>
+              <View
+                style={{
+                  backgroundColor: "#fff",
+                  alignItems: 'center',
+                  padding: 10,
+                  borderRadius: 10,
+                  borderColor: "#d3d3d3",
+                  borderWidth: 1
+                }}
+                key={index}>
+                <Text style={{color: "#000", marginBottom: 10}}>{item}</Text>
+                <Text style={{color: "#000"}}>{index}</Text>
+              </View>
+            </TouchableOpacity>
+          </>
+        ))}
+      </View>
+      <Text style={styles.textHeader}>Habits</Text>
       <FlatList data={activity} renderItem={renderItem}/>
       <TouchableOpacity onPress={() => {navigation.navigate("CreateHabit")}}>
         <View style={styles.floatingButton}>
@@ -51,8 +89,8 @@ const styles = StyleSheet.create({
     justifyContent: "center",
     alignItems: "center",
     position: "absolute",
-    bottom: 10,
-    right: 10,
+    bottom: 0,
+    right: 0,
     elevation: 5, // For Android shadow
     shadowColor: "#000", // For iOS shadow
     shadowOffset: { width: 0, height: 2 },
@@ -67,11 +105,15 @@ const styles = StyleSheet.create({
   list: {
     padding: 10,
     backgroundColor: Colors.white,
-    margin: 10,
-    borderRadius: 10
+    marginBottom: 5,
+    borderRadius: 10,
+    shadowOffset: { width: 0, height: 1 },
+    shadowOpacity: 0.1,
+    shadowRadius: 1,
   },
   textHeader: {
-    fontSize: 50,
+    fontSize: 30,
     fontWeight: "bold",
+    marginBottom: 10
   }
 });
