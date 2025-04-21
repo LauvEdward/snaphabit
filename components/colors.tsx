f9b53f43a242b66d6869a3332b775be2359fb21c
Le Dinh Phuc <phucld@ominext.com>
2025-04-21 16:12:53 +0700

    update color themes

--- components/colors.tsx
+++ components/colors.tsx
@@ -1,15 +1,15 @@
 const Colors = {
-  primary: '#3498db',
-  secondary: '#2ecc71',
-  danger: '#e74c3c',
-  warning: '#f39c12',
-  info: '#2980b9',
-  light: '#ecf0f1',
-  dark: '#2c3e50',
-  white: '#ffffff',
-  black: '#000000',
-  gray: '#95a5a6',
-  backgroundColor: "rgba(251, 242, 231, 1)"
+  deepGreen: '#0A6C4A',
+  darkTealGreen: '#056044',
+  mintGreen: '#12B886',
+  charcoal: '#202226',
+  gunmetal: '#242B35',
+  slateGray: '#464E5C',
+  coolGray: '#838D9E',
+  lightSlate: '#9FA6B2',
+  lightGray: '#D2D6DB',
+  veryLightGray: '#F2F2F2',
+  offWhite: '#FBFBFB'
 }
 
 export default Colors
